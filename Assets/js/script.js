dcbd66f1efa3fce471f85a044b07c7968742e1c8
MichaelKohronas <michaelckohronas@gmail.com>
2025-01-31 13:03:47 -0500

    Upload Fresh Portfolio Files

---
+++ Assets/js/script.js
@@ -0,0 +1,21 @@
+﻿// Window Scroll
+
+$(window).on("scroll", function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop >= 100) {
+        $('body').addClass('fixed-header');
+    } else {
+        $('body').removeClass('fixed-header');
+    }
+});
+
+//Document Ready
+
+$(document).ready(function () {
+    // Typing Animation
+    var typed = new Typed('#type-it', {
+        strings: ['Software Engineer', 'Student', 'Developer'],
+        typeSpeed: 250,
+        loop: true
+    });
+});
---
+++ README.md
@@ -0,0 +1,6 @@
+# Personal Portfolio
+This repository contains the code for my portfolio website.
+
+
+## __This website was created in conjunction with the following tutorial. I do not claim any of it as my own thinking. This website was created for fun, to learn more about Front-End Development languages, and to post my work somewhere other than GitHub__:  [Tutorial](https://www.youtube.com/watch?v=cZj0j7rCcek)
+## 
